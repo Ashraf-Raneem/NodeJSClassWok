1f249e63137661ba5ea642e30e9c2d9ef50ac852
Ashraf-Raneem <you@example.com>
2019-11-12 18:30:21 -0800

    classwork 8d

--- server.js
+++ server.js
@@ -2,25 +2,53 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var server = http.Server(app);
-var bodyParser = require('body-parser');
-
+var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.post('/new_article',function(request,response){
-  console.log(request.body);
-  response.json({msg: "Successfully recieved"})
+let articles= []
+
+app.post('/submit', function(request,response){
+  articles.push(request.body)
+  console.log(articles)
+  response.json({msg: "successfully received"})
 })
 
-app.get('/',function(request,response){
-  response.sendFile(__dirname+'/index.html')
-})
-app.get('/second',function(request,response){
-  response.sendFile(__dirname+'/index_2.html')
-})
-app.get('/new',function(request,response){
-  response.sendFile(__dirname+'/form.html')
+app.get('/article/:index', function(request, response){
+  if(articles[request.params.index]){
+      response.render('article.ejs', {article:articles[request.params.index]})
+  }else{
+    response.json({msg:"Article not available"})
+  }
+
 })
 
-server.listen(3000, 'localhost', function(){
-  console.log('Server running');
-});
+
+
+// app.post('/new_article', function(request, response){
+  
+//   console.log(request.body)
+//   response.json({msg: "Successfully received"})
+
+// })
+
+app.get('/', function(request, response){
+  response.sendFile(__dirname + '/views/index.html')
+
+})
+
+app.get('/second', function(request, response){
+  response.sendFile(__dirname + '/views/second.html')
+
+})
+
+app.get('/new', function(request, response){
+  response.sendFile(__dirname + '/views/form.html')
+
+})
+
+
+
+  
+  server.listen(3000, 'localhost', function(){
+    console.log('Server running');
+  });
