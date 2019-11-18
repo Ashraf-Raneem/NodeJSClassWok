7e864a48e8318bd1c19c4ace289b8d844db2ae9a
Ashraf-Raneem <you@example.com>
2019-11-17 19:13:57 -0800

    9d

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ var express = require('express');
 var app = express();
 var server = http.Server(app);
 var bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended:true}))
-var mongo = require('mongodb')
 
+//var mongo = require('mongodb')
+var mongoose = require('mongoose');
 var db, url  = "mongodb+srv://Ashraf:daredevils@cluster0-h5imv.mongodb.net/test?retryWrites=true&w=majority"
 
+mongoose.connect(url,
+  {useNewUrlParser: true, useUnifiedTopology: true})
+  mongoose.connection.on('error',function(err){
+    console.log("could not connect to mongoose"); 
+  })
+/*
 mongo.MongoClient.connect(url, 
   {useNewUrlParser: true, useUnifiedTopology: true},
   function(err,client){
@@ -26,14 +32,40 @@ var save = function(form_data){
   collection.save(form_data); 
   })
 }
+*/
 
+var Schema = mongoose.Schema
+var articleSchema = new Schema (
+    {
+      title: {
+          type: String,
+          required: "must require title"
+      },
+      content: {
+        type: String, 
+        required: "must require content"
+      }
+    }
+)
+
+var Article = mongoose.model('Article',articleSchema)
+app.use(bodyParser.urlencoded({extended:true}))
 var articles = [];
 
 app.post('/submit', function(request,response){
-  save(request.body)
-  articles.push(request.body)
-  console.log(articles)
-  response.json({msg: "successfully received"})
+  let article = new Article (request.body)
+  article.save(function(err,data){
+    if(err){
+      return response.status(400).json({msg: "All fields must be filled"})
+    }
+    return response.status(400).json({article:data});
+  })
+
+  
+  //save(request.body)
+ // articles.push(request.body)
+ // console.log(articles)
+ // response.json({msg: "successfully received"})
 })
 
 app.get('/article/:index', function(request, response){
